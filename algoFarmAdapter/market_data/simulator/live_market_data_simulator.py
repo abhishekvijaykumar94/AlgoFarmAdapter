0fa1d649794a4920da6559661d3c7866793fb310
abhishekvijaykumar94 <36844551+abhishekvijaykumar94@users.noreply.github.com>
2025-01-01 15:29:36 +0800

    @AngelBroking @MarketData @Simulator @Adding live market data simulator

---
+++ algoFarmAdapter/market_data/simulator/live_market_data_simulator.py
@@ -0,0 +1,88 @@
+import json
+import os
+import time
+from datetime import datetime
+
+import pandas as pd
+from algoLibs import DateRangeProcessor, BotoConnectionManager, CommonUtils, PropertyManager, AppConstants
+
+pd.set_option('display.max_colwidth', 1000)
+pd.set_option('display.max_columns', None)
+
+from confluent_kafka import Producer
+
+class LiveMarketDataSimulator(DateRangeProcessor):
+
+    def __init__(self, start_date, end_date, influx_client_manager, bucket_name, kafkaTopic):
+        super().__init__(start_date, end_date, influx_client_manager)
+        self.connection = BotoConnectionManager(bucket_name)
+        self.all_files = self.connection.list_files_in_bucket()
+        self.mktDataOutputDir = CommonUtils.getFilePathOutputDirectory()
+        self.producer_config = {
+            'bootstrap.servers': PropertyManager.getValue(AppConstants.BOOTSTRAP_SERVERS),
+            'batch.size': 163840,
+            'linger.ms': 100,
+            'max.in.flight.requests.per.connection': 10
+        }
+        self.marketDataProducer = Producer(self.producer_config)
+        self.marketDataProducer.flush(timeout=1)
+        self.kafkaTopic = kafkaTopic
+    def timestamp_to_datetime(self,timestamp):
+        return datetime.utcfromtimestamp(timestamp / 1000.0)
+
+    def publish_message(self,message):
+        json_string = json.dumps(message)
+        self.marketDataProducer.produce(self.kafkaTopic, json_string)
+        self.marketDataProducer.poll(0)
+
+    def process(self, date):
+        date_str = 'Market_Data_' + date.strftime("%Y%m%d") + '.7z'
+        for file_name in self.all_files:
+            if (date_str in file_name):
+                output_directory_path = os.path.join(self.mktDataOutputDir, file_name)
+                local_filename = file_name.split('/')[-1]
+                local_file_path = os.path.join(self.mktDataOutputDir, local_filename)
+                current_time = datetime.now()
+                if not os.path.exists(local_file_path):
+                    print(f'Downloading {file_name} to {local_filename}')
+                    self.connection.download_object(file_name, output_directory_path)
+                    CommonUtils.extract_data_from_7zip_to_current_path(output_directory_path, self.mktDataOutputDir)
+                else:
+                    print(f'File {local_filename} already exists, skipping download.')
+                CommonUtils.copyFile(self.mktDataOutputDir,CommonUtils.getPathToProjectDirectory("data"),"Token_mapping.csv")
+                all_pkl_files = [f for f in os.listdir(self.mktDataOutputDir) if
+                                     f.endswith('.pkl')]
+                all_pkl_files.sort(key=lambda f: int(f.split('example')[1].split('.')[0]))
+                previous_timestamp = None
+                for dbfileName in all_pkl_files:
+                    message_count = 0
+                    if dbfileName.endswith('.pkl'):
+                        # print("Publishing all messages for file: ", dbfileName)
+                        dbfile = CommonUtils.load_pickle_file(self.mktDataOutputDir,'/'+dbfileName)
+                        print("Timestamp of the first message in the file ", dbfileName, " is ", current_time," actual time ", datetime.now())
+                        for i, message in enumerate(dbfile):
+                            message_count += 1
+                            timestamp = message['exchange_timestamp']
+                            current_time = self.timestamp_to_datetime(timestamp)
+                            if previous_timestamp is None:
+                                previous_timestamp = current_time
+                            # if i == 0:  # Set the start time and previous timestamp for the first message in the dbfile
+                            #
+                            #     previous_timestamp = current_time
+                            # else:
+                            if current_time < previous_timestamp:
+                                time_diff_seconds = 0
+                            else:
+                                time_diff = current_time - previous_timestamp
+                                time_diff_seconds = time_diff.total_seconds()
+                            if time_diff_seconds > 0:
+                                if(time_diff_seconds>30.0):
+                                    print("Putting thread to sleep for ",time_diff_seconds," seconds")
+                                time.sleep(time_diff_seconds)
+                            self.publish_message(message)
+                            if previous_timestamp <= current_time:
+                                previous_timestamp = current_time
+
+                            # if message_count % 5000 == 0:
+                            #     print("Total message count at ", timestamp, " for file ", dbfileName, " is ",message_count)
+
